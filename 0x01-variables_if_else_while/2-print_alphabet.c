5d6cfed9a5de859f95d1eacf15b48a9d323e6b6a
Atenea Castillo Luna <69764092+AteCastillo@users.noreply.github.com>
2020-09-17 19:47:42 -0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,13 +3,10 @@
 *main - Entry point
 *Return: Always 0 (Success)
 */
-
 int main(void)
-
 {
 char ch;
-
-	for (ch = a ; ch <= z ; ch++)
+	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{	
 		putchar(ch);
 	}
