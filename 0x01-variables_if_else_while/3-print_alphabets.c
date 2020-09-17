7fa9fac782e1e4680a1ae5aae96b5eab85e07808
Atenea <atecast@gmail.com>
2020-09-17 23:56:17 +0000

    new

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,19 +3,3 @@
  *main - Entry point
  *Return: Always 0 (Success)
  */
-
-int main(void)
-{
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-putchar(ch);
-}
-char chA;
-for (chA = 'A' ; chA <= 'Z' ; chA++)
-{
-putchar(chA);
-}
-putchar (\n);
-return (0);
-}
