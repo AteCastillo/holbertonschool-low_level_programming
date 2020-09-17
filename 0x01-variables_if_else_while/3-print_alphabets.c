f52baf8ba17d34a7b54df7f3f0b8d29334a0bcae
Atenea Castillo Luna <69764092+AteCastillo@users.noreply.github.com>
2020-09-17 20:18:31 -0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,15 +6,16 @@
 int main(void)
 {
 int ch;
-
-        clrscr();
-	for (ch= 'a'; ch<= 'z'; ch++)
+       
+	for (ch= 'a'; ch<= 'z'; ch++) {
 	
              putchar(ch);
+	}
 	
-	for (ch= 'A'; ch<= 'Z'; ch++)
+	for (ch= 'A'; ch<= 'Z'; ch++) {
 	
              putchar(ch);
+	}
 
 	putchar (\n);
 
