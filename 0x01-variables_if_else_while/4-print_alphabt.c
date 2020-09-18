a4028f92bfb3a5ddc85951ffbb8371b891546039
Atenea <atecast@gmail.com>
2020-09-18 00:09:28 +0000

    fix

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,14 +7,13 @@
 int main(void)
 {
 	char ch;
-
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
 		if (ch != 'e' && ch != 'q')
-			}
-	{
-		putchar (ch);
+		{
+			putchar(ch);
+		}
 	}
-	putchar ('\n');
+	putchar('\n');
 	return (0);
 }
