ce85235034fcf7d369316c367482d6a4e8c4567d
Atenea <atecast@gmail.com>
2020-09-18 00:32:56 +0000

    new

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- *main - Entry point
- *Return: Always 0 (Success)
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char ch;
+
 	for (ch = 'z' ; ch >= 'a' ; ch--)
 	{
 		putchar(ch);
