d7317c63a0e84f18226a8288183c9d7bb4c897be
Atenea <atecast@gmail.com>
2020-09-18 00:56:06 +0000

    new

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
-
+ * main - Entry point
  *Return: Always 0 (Success)
 */
 
 int main(void)
 {
-        int nu;
+	char nu;
 
-        for (nu = '0' ; nu <= '9' ; nu++)
-        {
-                putchar(nu);
-        }
-        putchar('\n');
-        return (0);
+	for (nu = '0' ; nu <= '9' ; nu++)
+	{
+		putchar(nu);
+	}
+	putchar('\n');
+	return (0);
 }
