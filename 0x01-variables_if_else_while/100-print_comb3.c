0b90ccce92be7a2cf7bba09b49bb7905cf1bb620
Atenea <atecast@gmail.com>
2020-09-19 01:47:04 +0000

    fix

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int ch, nu;
+
+	for (ch = '0' ; ch <= '9' ; ch++)
+	{
+		for (nu = '0' ; nu <= '9' ; nu++)
+		{
+			if (ch == nu || ch > nu)
+			{
+				continue;
+			}
+				putchar(ch);
+				putchar(nu);
+			       	if (ch != '8' || nu != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
