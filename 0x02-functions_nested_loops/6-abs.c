03116b72413c3fe703325e702d17f03a43bd4c94
Atenea <atecast@gmail.com>
2020-09-22 12:48:36 +0000

    new

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int nu)
+
+{
+
+	return (nu * ((nu > 0) - (nu < 0)));
+}
