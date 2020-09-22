7cfb095be9606a60925576aa360fb38711718824
Atenea <atecast@gmail.com>
2020-09-22 23:28:16 +0000

    m

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,20 +12,20 @@ void times_table(void)
 	{
 		for (j = 48; j <= 57; j++)
 		{
-			res = (i - 48) * (j - 48);
-			sig = (i - 48) * ((j + 1) - 48);
-			if (res > 9)
+
+
+
 			{
 				_putchar((res / 10) + 48);
 				_putchar((res % 10) + 48);
 			}
-			else
+
 				_putchar(res + 48);
 			if (j != 57)
 			{
 				_putchar(',');
-				if (res <= 9 && sig <= 9 && j <= 57)
-					_putchar(' ');
+
+
 				_putchar(' ');
 			}
 		}
