a7b2741d12adfe0d4379de3c4ad94b93a5f537df
Atenea <atecast@gmail.com>
2020-09-22 13:25:18 +0000

    fix

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,8 +3,9 @@
 
 /**
  * add - adds two integers and returns the result
- * @n: int received
- * Return: the result.
+ * @a: int 1
+ * @b: int 2
+ * Return: the result
  */
 
 int add(int a, int b)
