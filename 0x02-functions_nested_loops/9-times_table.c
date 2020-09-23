3fd5937c83ace1de399998345c67e3f93347c3db
Atenea <atecast@gmail.com>
2020-09-23 20:11:11 +0000

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,8 +19,8 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(res /10 + '0');
-				_putchar(res %10 + '0');
+				_putchar(res / 10 + '0');
+				_putchar(res % 10 + '0');
 			}
 			else
 			{
