4ebc69adeb76136b23dbd19ef7bfa0d7cab3c93a
Atenea <atecast@gmail.com>
2020-09-24 19:05:55 +0000

    new

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: int received
+ * Return: None.
+ */
+
+void print_diagonal(int n)
+
+{
+	int i, linea;
+
+	if (n > 0)
+	{
+		for (linea = 1; linea <= n; linea++)
+		{
+			for (i = 0; i < linea; i++)
+			{
+				_putchar(' ');
+			}
+			{
+				_putchar('\\');
+				_putchar('\n');
+			}
+		}
+	}
+
+	else
+	{
+		_putchar('\n');
+	}
+}
