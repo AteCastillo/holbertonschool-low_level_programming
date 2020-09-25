b1dcf55aa256b1423e3c73ed2c5b5ac130dbe616
Atenea <atecast@gmail.com>
2020-09-25 01:25:34 +0000

    new

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_triangle - print a triangle
+ * @size: the size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i <= size - 1; i++)
+		{
+			for (j = 0; j <= size - 1; j++)
+			{
+				if ((i + j) >= size - 1)
+					_putchar('#');
+				else
+					_putchar(' ');
+			}
+			_putchar(10);
+		}
+	}
+	else
+		_putchar(10);
+}
