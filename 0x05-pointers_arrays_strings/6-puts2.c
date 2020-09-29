401480a4329b21075772e090793b406cfb0a0e33
Atenea <atecast@gmail.com>
2020-09-29 16:11:15 +0000

    new

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: var received
+ * Return: none.
+ */
+
+
+void puts2(char *str)
+{
+	int n; /*n to count number of characters in the string */
+
+	for (n = 0; str[n] != '\0'; n++)
+	{
+		if (n % 2 == 0)
+		{
+			_putchar(str[n]);
+		}
+        }
+/*	while (str[n] != '\0')
+	{
+		n++;
+
+
+	if (n % 2 == 0)
+	{
+		_putchar(str[n]);
+	}
+	}
+*/
+	_putchar('\n');
+
+}
