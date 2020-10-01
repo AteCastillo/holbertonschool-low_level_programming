612aef6a5808fb95cc9b659c68e85c3693a78343
Atenea <atecast@gmail.com>
2020-10-01 16:18:08 +0000

    new

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: var received
+ * @scr: var received
+ * Return: Always 0.
+ */
+
+
+char *_strcat(char *dest, char *src)
+{
+
+	int n, temp;
+
+	for (n = 0; dest[n] != '\0'; dest++)
+	{
+	}
+
+	for (temp = 0; src[temp] != 0; ++temp, ++n)
+	{
+		dest[n] = src[temp];
+	}
+	dest[n] = '\0'; /*to add null zero */
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+
+/**
+ * _strncpy - a function that copies a string.
+ * @dest: var received
+ * @src: var received
+ * @n: var received
+ * Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	}
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return dest;
+}
