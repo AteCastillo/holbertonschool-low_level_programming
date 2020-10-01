43d2d8ec82e80ca46fa79c47033e8fa3af824ae7
Atenea <atecast@gmail.com>
2020-10-01 18:34:46 +0000

    new

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+
+void reverse_array(int *a, int n)
+
+{
+	int i, temp, x;
+
+	x = n - 1;
+	for (i = 0; a[i] < x; i++)
+	{
+		temp = a[x];
+		a[x] = a[i];
+		a[i] = temp;
+		x--;
+	}
+}
