b834a592e3c3975bbc714c847876702e4cceae1a
Atenea Castillo Luna <69764092+AteCastillo@users.noreply.github.com>
2020-10-02 15:57:28 -0300

    Update 8-rot13.c

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,1 +1,28 @@
+#include "holberton.h"
+
+
+/**
+ * leet - check the code for Holberton School students.
+ * @s: string
+ * Return: Always 0.
+ */
+
+char *leet(char *s)
+{
+	int n, x;
+	char abc[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char abc2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for(n = 0; s[n] != '\0'; n++)
+	{
+		for(x = 0; abc[x] != '\0'; x++)
+		{
+			if (abc2[x] == s[n])
+			{
+				s[n] = abc2[x];
+			}
+		}
+	}
+	return (s);
+}
 
