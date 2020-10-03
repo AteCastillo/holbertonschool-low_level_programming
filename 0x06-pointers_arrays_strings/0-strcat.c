62aca479a7f7c8b40ddfc2bd192f094095f748c1
Atenea Castillo Luna <69764092+AteCastillo@users.noreply.github.com>
2020-10-02 22:51:25 -0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,26 +1,24 @@
 #include "holberton.h"
+#include <stdio.h>
+
 /**
- * _strcat - concatenates two strings
- * @dest: var received
- * @scr: var received
+ * _strcat - check the code for Holberton School students.
+ * @dest: abc
+ * @src: abc
  * Return: Always 0.
  */
-
-
-	char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
-	int dest_len;
-	int i;
-
-	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
-	{
-	}
-
-	for (i = 0; src[i] != 0; i++)
-	{
-		dest[dest_len + i] = src[i];
-		dest[dest_len + i] = '\0';
-	}
-
-	return (dest);
+int n, temp;
+n = 0;
+while (dest[n] != '\0')
+{
+++n;
+}
+for (temp = 0; src[temp] != '\0'; ++temp, ++n)
+{
+dest[n] = src[temp];
+}
+dest[n] = '\0';
+return (dest);
 }
