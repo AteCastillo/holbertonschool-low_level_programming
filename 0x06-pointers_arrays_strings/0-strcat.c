541f0fb5e57c4a24f3b2aa8b9f021b8299c5154e
Atenea Castillo Luna <69764092+AteCastillo@users.noreply.github.com>
2020-10-02 21:08:17 -0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -18,8 +18,8 @@ char *_strcat(char *dest, char *src)
 
 	for (temp = 0; src[temp] != 0; temp++)
 	{
-		dest[n] = src[temp];
-		dest[n] = '\0';
+		dest[n + i] = src[temp];
+		dest[n + i] = '\0';
 	}
 	 /*to add null zero */
 	/* for (; src[i] != '\0'; n++)
