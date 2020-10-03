f292670e372f0eed0cb8e7545777e19ac1e1fce0
Atenea Castillo Luna <69764092+AteCastillo@users.noreply.github.com>
2020-10-02 22:55:29 -0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,29 @@
 #include "holberton.h"
 
-
 /**
- * cap_string - check the code for Holberton School students.
+ * cap_string - capitalizes a string
  * @s: string
- * Return: Always 0.
+ * Return: string
  */
 
 char *cap_string(char *s)
 {
-	int n, x;
-	
-	if (s[0] >= 97 && s[0] <= 122)
+	int i;
+
+	if (s[0] >= 'a' && s[0] <= 'z')
 	{
 		s[0] = s[0] - 32;
 	}
-	for (n = 0; s[n] != '\0'; n++)
+	for (i = 1; s[i] != '\0'; i++)
 	{
-		x = n - 1;
-		if ((s[n] >= 97 && s[n] <= 122) && 
-		    (s[x] == 32 || s[x] == 33 || s[x] == 34 || 
-		     s[x] == 9 ||  s[x] == 44 || s[x] == 40 || 
-		     s[x] == 41 || s[x] == 123  || s[x] == 125 ||  s[x] == 46))
+		if ((s[i - 1] == ' ' || s[i - 1] == '\n' || s[i - 1] == '\t'
+		     || s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '!'
+		     || s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '('
+		     || s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}'
+		     || s[i - 1] == '.') && (s[i] >= 'a' && s[i] <= 'z'))
 		{
-
-			s[n] = s[n] - 32;
+			s[i] = s[i] - 32;
 		}
 	}
 	return (s);
-	
 }
