e858d0c675f64af605c4b9949f4373c82fb86b03
Atenea Castillo Luna <69764092+AteCastillo@users.noreply.github.com>
2020-10-04 12:10:55 -0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -20,15 +20,17 @@ void puts2(char *str)
 	}
 
 /*
+ *	OTHER WAY TO DO IT
  *	while (str[n] != '\0')
  *	{
- *		n++;
  *
  *
  *	if (n % 2 == 0)
  *	{
  *		_putchar(str[n]);
+ *	
  *	}
+ *	n++;
  *	}
  */
 	_putchar('\n');
