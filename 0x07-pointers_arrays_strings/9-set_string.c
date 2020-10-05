53850992e7d06c20f50c3f3e00b99267eea9852f
Atenea <atecast@gmail.com>
2020-10-05 23:37:28 +0000

    new

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * set_string - a function that locates a character in a string
+ * @s: pointer
+ * @to: char received
+ * Return:the pointer to char or 0 if dont have coincidence
+ */
+
+void set_string(char **s, char *to)
+{
+	
+	**s = *to;
+}
