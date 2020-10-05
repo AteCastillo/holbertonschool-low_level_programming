61b032bd93fd84fd561f2b39b9dccca1cf0492f0
Atenea <atecast@gmail.com>
2020-10-05 17:52:00 +0000

    fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,4 +8,19 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
+	int i, n, x = 0;
 
+	for (i = 0; s[i] != 32; i++)
+	{
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (s[i] == accept[n])
+				x++;
+			/*	else */
+			/*	break; */
+			/*if it finds something different */
+			/* it breaks the loop */
+		}
+	}
+	return (x);
+}
