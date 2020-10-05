a103a9ce3ef04a2a1143c9a511139a144a9827c8
Atenea <atecast@gmail.com>
2020-10-05 18:06:01 +0000

    fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 
 /**
- * int _strspn - a function that gets the length of a prefix substring
- *
+ * _strspn - a function that gets the length of a prefix substring
+ * @s: pointer
+ * @accept: char received
  * Return: Always 0.
  */
 
@@ -10,17 +11,19 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i, n, x = 0;
 
-	for (i = 0; s[i] != 32; i++)
+	for (i = 0; s[i] != 0; i++)
 	{
 		for (n = 0; accept[n] != '\0'; n++)
 		{
 			if (s[i] == accept[n])
+			{
 				x++;
-			/*	else */
-			/*	break; */
+			}
+		}
+		if (x <= i)
+			break;
 			/*if it finds something different */
 			/* it breaks the loop */
-		}
 	}
 	return (x);
 }
