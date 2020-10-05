3173b8775324ea4a53c794974951521696f41f7f
Atenea <atecast@gmail.com>
2020-10-05 20:07:43 +0000

    new

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _strstr - a function that locates a character in a string
+ * @haystack: pointer
+ * @needle: char received
+ * Return:the pointer to char or 0 if dont have coincidence
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, n;
+
+	for (n = 0; haystack[n]; n++)
+	{
+		if (needle[0] == haystack[i])
+		{
+			for (i = 0; needle[i] != '\0' &&
+			haystack [i + n] != '\0'; i++)
+			{
+				if (needle[i] != haystack[i + n])
+				break;
+			}
+		}
+		if (needle[i + 1] == '\0')
+			return (haystack + i);
+	}
+	return ('\0');
+}
