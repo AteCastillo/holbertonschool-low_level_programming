fc620d293e98b801c589cf2826e19d86b3a04419
Atenea <atecast@gmail.com>
2020-10-05 16:50:08 +0000

    new

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - a function that copies memory area
+ * @dest: pointer
+ * @src: pointer
+ * @n: unsigned int received
+ * Return: Nothing.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
