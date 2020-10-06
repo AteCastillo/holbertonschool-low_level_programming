2e920d774bb820e384f4e6138297cac49902e39a
Atenea <atecast@gmail.com>
2020-10-06 17:13:44 +0000

    new

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,42 +3,25 @@
 /**
  * print_diagsums - a function that locates a character in a string
  * @a: pointer
- * @size: char received
+ * @size: int received
  * Return:the pointer to char or 0 if dont have coincidence
  */
 
 void print_diagsums(int *a, int size)
 
 {
-/*
- *	int row, column, sum = 0;
- *	for (row = 0; a[row] < size ; row++)
- *        {
- *		for (column = 0; a[column]; column++)
- *		{
- *			if (row == column)
- *			{
- *				sum += a[row][column];
- *				printf("%d",sum);
- *			}
- *		}
- *	}
- *
- *	printf("\n");
- */
-	int i, j, p, l = 0, r = 0;
 
-	for (i = 0; i < size; i++)
-	{
-		p = (i * size) + i;
-		l += *(a + p);
-	}
-	for (j = 0; j < size; j++)
-	{
-		p = (j * size) + (size - 1 - j);
-		r += *(a + p);
-	}
-	printf("%i, %i\n", l, r);
+	int row, column, sum1, sum2;
 
+	for (row = 0, column = 0; row < size ; row++, column++)
+        {
+		sum1 += a[row][column];
+	}
+
+	for (column = size - 1, row = 0; row < size; row++, column--)
+	{
+		sum2 += a[row][column];
+	}
+	printf("%d, %d\n", sum1, sum2);
 
 }
