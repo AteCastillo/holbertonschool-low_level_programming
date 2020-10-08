08588ce1669272d4b1fb5298b8da30108cb98762
Atenea <atecast@gmail.com>
2020-10-08 02:23:10 +0000

    new

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,39 +1,75 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- * _sqrt_recursion - check the code for Holberton School students.
- * @n: var received
- * Return: Always 0.
+ *_sqrt_recursion - blank
+ * @n: blank
+ * Return: balnk
  */
-
 
 int _sqrt_recursion(int n)
 {
+	if (n == 0)
+		return (0);
+	else if (n == 1)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (function(n, 1));
+}
+
+/**
+ * function - blank
+ * @n: number
+ * @i: incrementor
+ * Return: -1;
+ */
+
+int function(int n, int i)
+{
+	if (n == (i * i))
+		return (i);
+	else if (n > (i * i))
+		return (function(n, i + 1));
+	else
+		return (-1);
+}
+
+/*
+ *#include "holberton.h"
+ * _sqrt_recursion - check the code for Holberton School students.
+ * @n: var received
+ * Return: Always 0.
+ *
+
+
+int _sqrt_recursion(int n)
+
+{
 	if (n < 0)
 		return (-1);
-	if (n == 0)
+	else if (n == 0)
 		return (1);
 	else
 		return (function(n, 1));
 
 }
 
- /**
- * function - find the sqrt of a number
- * @x: result
- * @i: base
- * Return: the base of a sqrt
- */
+ *
+ * function - square root of a number
+ * @n: result
+ * @i: multiplos
+ * Return: -1
+ *
 
-int function(int i, int x)
+int function(int i, int n)
+
 {
-	if (i * i == x)
+	if ((i * i) == n)
 		return (i);
-	if (i * i > x)
+	if ((i * i) > n)
 		return (-1);
 	else
-		return (funtion(x, i + 1));
+		return (function(n, i + 1));
 }
-
-}
+*/
