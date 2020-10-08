0eb86d0251b7e934aebe9c3729008bcc12bd6afb
Atenea Castillo Luna <69764092+AteCastillo@users.noreply.github.com>
2020-10-07 23:30:22 -0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,9 +11,31 @@ int is_prime_number(int n)
 
 {
 
-
-
-
-
-
+  if (n == 0)
+		return (0);
+	else if (n < 0)
+		return (-1);
+	else
+		return (function(n, 1));
 }
+
+/**
+ * function - blank
+ * @n: number
+ * @i: incrementor
+ * Return: -1;
+ */
+
+int function(int n, int i)
+{
+
+  if(i%2!=0 && i%3!=0 && i%5!=0 && i%7!=0 || i==2 || i==3 || i==5 || i==7)
+		return (function(n, i + 1));
+	else
+		return (0);
+}
+
+
+
+
+
