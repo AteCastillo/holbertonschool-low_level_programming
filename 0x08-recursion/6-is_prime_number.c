39056d270fbf8c48fdce0d20367c3946bf359c4a
Atenea Castillo Luna <69764092+AteCastillo@users.noreply.github.com>
2020-10-07 23:32:15 -0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * is_prime_number - check the code for Holberton School students.
@@ -29,7 +28,7 @@ int is_prime_number(int n)
 int function(int n, int i)
 {
 
-  if(i%2!=0 && i%3!=0 && i%5!=0 && i%7!=0 || i==2 || i==3 || i==5 || i==7)
+  if(i %2 != 0 && i %3 != 0 && i %5 != 0 && i %7 != 0 || i == 2 || i == 3 || i == 5 || i == 7)
 		return (function(n, i + 1));
 	else
 		return (0);
