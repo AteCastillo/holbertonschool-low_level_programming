1f3c2ca0273b72cc3da9ea3a4f1bf8399704e93a
Atenea <atecast@gmail.com>
2020-10-10 21:59:52 +0000

    repair 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,12 +4,12 @@
  * main - function
  * @argc: argument
  * @argv: array
- * return: 0.
+ * Return: 0.
  */
 
 int main(int argc, char *argv[])
 {
-	(void) argc;
+	(void)argc;
 
 	printf("%s\n", argv[0]);
 	return (0);
