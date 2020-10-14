e663329abdef01ce22298b6d544149c2266af056
Atenea <atecast@gmail.com>
2020-10-14 21:12:27 +0000

    new

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,8 +21,10 @@ int **alloc_grid(int width, int height)
 
 	array = (int **) malloc(height * sizeof(int));
 	if (array == NULL)
+	{
+		free(array);
 		return (NULL);
-	free (array);
+	}
 	for (i = 0; i < height; i++)
 	{
 		array[i] = (int *) malloc(width * sizeof(int));
