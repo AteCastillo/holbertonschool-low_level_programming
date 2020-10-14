b8b16edc5d3a3713ea7e7b5d4c3000721465d402
Atenea <atecast@gmail.com>
2020-10-14 21:02:18 +0000

    new

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - prints a grid of integers
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+
+
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int i;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	array = (int **) malloc(height * sizeof(int));
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		array[i] = (int *) malloc(width * sizeof(int));
+		{
+			if (array[i] == NULL)
+			{
+				for (i = -1; i > 0; i--)
+				{
+					free(array[i]);
+				}
+				free(array);
+				return (NULL);
+			}
+		}
+	}
+
+
+	return (array);
+}
