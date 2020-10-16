571870518b7f793740d4dad5817c28b94fac105b
Atenea <atecast@gmail.com>
2020-10-16 01:42:13 +0000

    fix

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strtow - Prints an array of string
+ * @str: The array to print
+ *
+ * Return: nothing
+ */
+
+char **strtow(char *str)
+
+{
+
+
+
+}
---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - check the code for Holberton School students.
+ * @ac: int received
+ * @av: pointer to pointer
+ * Return: Always 0.
+ */
+
+char *argstostr(int ac, char **av)
+{
+
+
+
+}
