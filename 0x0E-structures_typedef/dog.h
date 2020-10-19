bd2ff20b086faecf2d20cd12dc59c6684b698858
Atenea <atecast@gmail.com>
2020-10-19 22:17:16 +0000

    fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,8 +14,8 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} doggi;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
+void print_dog(struct dog *d);
 #endif
