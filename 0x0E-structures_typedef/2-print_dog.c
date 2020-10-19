f2eeb9c3abd530c15db03cfa87aa5de82b95e639
Atenea <atecast@gmail.com>
2020-10-19 22:28:13 +0000

    fix2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,14 +10,14 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		return;
-	if (d != NULL)
 	{
+		return;
+	}
 	if (d->name != NULL)
 		printf("Name: %s\n", d->name);
 	else
 		printf("Name: (nil)\n");
-	if (d->age >= 0)
+	if (d->age > 0)
 		printf("Age: %f\n", d->age);
 	else
 		printf("Age: (nil)\n");
@@ -25,5 +25,5 @@ void print_dog(struct dog *d)
 		printf("Owner: %s\n", d->owner);
 	else
 		printf("Owner: (nil)");
-	}
+
 }
