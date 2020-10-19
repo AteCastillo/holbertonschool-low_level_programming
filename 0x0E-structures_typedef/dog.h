c94eea82895bed736c9c00ee66458738f26b66a4
Atenea <atecast@gmail.com>
2020-10-19 18:40:56 +0000

    new

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct - check the code for Holberton School students.
+ * @name: string for name
+ * @age: int for age
+ * @owner: string for owner
+ * Return: Always 0.
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} doggi;
+
+#endif
