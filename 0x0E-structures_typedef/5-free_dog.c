814ca03063e61527e127acc0bed3469135657b0f
Atenea <atecast@gmail.com>
2020-10-20 02:52:40 +0000

    new

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void free_dog(dog_t *d)
+
+{
+
+	if (d == NULL)
+		return;
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
