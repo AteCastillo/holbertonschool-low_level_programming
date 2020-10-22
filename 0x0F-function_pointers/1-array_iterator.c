438c3572c18e3a3988e153d5bcb41bc7eefa7ac4
Atenea Castillo Luna <69764092+AteCastillo@users.noreply.github.com>
2020-10-21 23:13:47 -0300

    Create 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * array_iterator - iterate in the array and execute the function action
+ * @array: the array
+ * @size: the size of the array
+ * @action: the function of action
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	if (size == 0)
+		return;
+	if (array == NULL)
+		return;
+	if (action == NULL)
+		return;
+	for (i = 0; i < (int)size; i++)
+		action(array[i]);
+}
