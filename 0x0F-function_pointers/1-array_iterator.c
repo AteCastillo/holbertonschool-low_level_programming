4153cf170f1f5fc6a1f8b38bdd6a210b82a01c29
Atenea <atecast@gmail.com>
2020-10-22 01:57:31 +0000

    new

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -19,8 +19,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (action == NULL)
 		return;
-	if (size == NULL)
-		return;
 
 	for (n = 0; n < size; n++)
 		(*action)(array[n]);
