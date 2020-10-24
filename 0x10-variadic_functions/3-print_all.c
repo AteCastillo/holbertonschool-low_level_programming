f2e7875219428d57fc0affc899a3fbee8f03e23f
Atenea <atecast@gmail.com>
2020-10-24 22:39:41 +0000

    new

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,57 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 #include "variadic_functions.h"
+
+
+/**
+ * print_char - print
+ * @n: char received
+ * Return: None
+ */
+
+void print_char(char n)
+
+{
+	printf("%c", n);
+}
+
+/**
+ * print_int - print int
+ * @n: The operator
+ * Return: None
+ */
+
+void print_int(int n)
+
+{
+        printf("%i", n);
+}
+
+
+/**
+ * print_string - check the code for Holberton School students.
+ * @n: string
+ * Return: None.
+ */
+void print_string(char *n)
+
+{
+        printf("%s", n);
+}
+
+/**
+ * print_float - check the code for Holberton School students.
+ * @n: float
+ * Return: None.
+ */
+
+void print_float(float n)
+
+{
+        printf("%f", n);
+}
+
 
 /**
  * print_all - check the code for Holberton School students.
@@ -11,7 +62,28 @@
 
 void print_all(const char * const format, ...)
 {
+	char *s;
+	int i;
+	op_t new[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"s", print_string},
+		{"f", print_float},
+		{NULL, NULL}
+	}
+
+	va_list newlist;
+	va_start(newlist, 4)
+		while (i = 0, i < 4;)
+
+
 
 
 
 }
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,18 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *s;
+	void (*f)();
+} op_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
