dfa13ca0b0575803052c26998433a057b9671f1d
Atenea <atecast@gmail.com>
2020-10-25 00:02:37 +0000

    fix

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,18 +1,13 @@
-global _start
-
-	section .text
-
-_start:
-	  mov rax, 1
-	  mov rdi, 1
-	  mov rsi, msg
-	  mov rdx, 17
-	  syscall
-
-	  mov rax, 60
-	  mov rdi, 0
-	  syscall
-
-	section .rodata
-msg:	 db "Hello, Holberton", 10
-msglen:	 equ $ - msg
+global main
+	        section .text
+main:
+	        mov     rax, 1
+	        mov     rdi, 1
+	        mov     rsi, msg
+	        mov     rdx, 17
+	        syscall
+	        mov     rax, 60
+	        mov     rdi, 0
+	        syscall
+	        section .data
+	        msg db "Hello, Holberton", 10
