0abef6c5b89d6f0ee6cc5621b8ae0c3d5e21fc7e
Atenea <atecast@gmail.com>
2020-10-30 01:22:51 +0000

    fix

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,17 @@
- SECTION .data
+section .data
 msg:	    db "Hello, Holberton", 0
 fmt:	    db "%s", 10, 0
 
-	        SECTION .text
-	        extern printf
-	        global main, _start
+section .text
+	extern printf
+	global main, _start
 main:
 _start:
-	        mov esi, msg
-	        mov edi, fmt
-	        mov eax, 0
-	        call printf
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
-	        mov ebx, 0
-	        mov eax, 1
-	        int 0x80
+	mov ebx, 0
+	mov eax, 1
+	int 0x80
