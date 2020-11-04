fac55774b26dc7e569b9605a82f4d44b086fd716
Atenea <atecast@gmail.com>
2020-11-04 13:13:23 +0000

    new

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,8 +11,13 @@ void free_listint2(listint_t **head)
 	listint_t *new; /*temp to hold what is inside head */
 
 	if (*head == NULL)
-		free(*head);
 		return;
+	while (head)
+	{
+		auxiliar = head->next;
+		free(head);
+		head = auxiliar;
+	}
 
 	while (*head)
 	{
