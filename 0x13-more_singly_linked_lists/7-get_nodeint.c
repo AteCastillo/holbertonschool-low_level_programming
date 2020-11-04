31fe4557415e90e2c49f214f439fede3a155bb92
Atenea <atecast@gmail.com>
2020-11-04 13:41:31 +0000

    fix

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,9 +18,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (i < index)
 	{
 		auxiliar = head->next;
+		head = head->next;
 		i++;
-		if (auxiliar == NULL)
-			return (NULL);
 	}
 	return (auxiliar);
 }
