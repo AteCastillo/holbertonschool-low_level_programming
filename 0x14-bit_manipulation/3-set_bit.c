f0f41fbf1cd2cd9e25a44981ba284ecbf9410609
Atenea <atecast@gmail.com>
2020-11-06 01:15:51 +0000

    fix

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * set_bit - check the code for Holberton School students.
+ * @n: unsigned long int
+ * @index: unsigned int
+ * Return: int.
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+
+
+}
---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - check the code for Holberton School students.
+ * @n: unsigned long
+ * @index: unsigned int
+ * Return: Always 0.
+ */
+
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+
+}
