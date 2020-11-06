432a22c26757b84bfdbb3ef1973d543d048293f2
Atenea <atecast@gmail.com>
2020-11-06 19:04:40 +0000

    fix

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,7 +9,11 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	if (index > sizeof(unsigned long int) * 8) /* a long int has 8 bytes,
+						      this has to be multiplied
+						      by 8 bites */
+	      return (-1);
 
-
-
+	*n = *n | (1 << index);
+	return (1);
 }
