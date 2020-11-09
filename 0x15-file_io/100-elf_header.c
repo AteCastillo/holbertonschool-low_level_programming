544dd489062ea9c601bdfbff5a581d2661721aa7
Atenea <atecast@gmail.com>
2020-11-09 22:48:45 +0000

    fix

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: count
+ * @argv: arguments
+ * Return: Always 0.
+ */
+
+int main (int argc, char argv[])
+{
+
+}
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * copy_toanother_file - check the code for Holberton School students.
+ * @argc: pointer to name of the file
+ * @argv: pointer to pointer of the file
+ * Return: Always 0.
+ */
+int main(int argc, char argv[])
+{
+	int fd, buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+	
+	if( 
+
+
+}
