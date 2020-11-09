54f97e1b926f0faad928f820f15adb899d199145
Atenea <atecast@gmail.com>
2020-11-09 19:45:54 +0000

    new

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * create_file - check the code for Holberton School students.
+ * @filename: name of the file
+ * @text_content: string to writeo the file
+ * Return: Always 0.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+
+
+
+}
