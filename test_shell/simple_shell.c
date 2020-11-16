1bdba74b387a96746074fd3aac60e8842dec76b5
Atenea <atecast@gmail.com>
2020-11-16 20:55:33 +0000

    new

--- test_shell/simple_shell.c
+++ test_shell/simple_shell.c
@@ -13,16 +13,17 @@
 
 int main(void)
 {
-	char *buffer = malloc(1024);
+	char *buffer = NULL;
 	char **argv = malloc(1024);
 	size_t size = 0;
-	int get, c, i=0, status;
-	char *ptr, delim = ' ';
+	int get, i, status; /*get is the return from getline*/
+	char *ptr;
 	pid_t child;
 
 	while (1)
 	{
 		write (STDOUT_FILENO, "prompt$ ", 8);
+		buffer = malloc(1024); /*everytime a new word is entered it has to be an allocation*/
 		get = getline(&buffer, &size, stdin);
 		if (get == -1)
 		{
@@ -30,42 +31,48 @@ int main(void)
 			{
 				free(buffer);
 				free(argv);
-				break;
-				write (1, "\n", 1);
+				write(1, "\n", 1);
 				return (1);
 			}
 			perror("error");
 			free(buffer);
 			free(argv);
+
 			return(1);
 		}
-	ptr = strtok(buffer, &delim);
-	while (ptr)
-	{
-		argv[i] = ptr;
-		ptr = strtok(NULL, &delim);
-		if (ptr != NULL)
-			write(STDOUT_FILENO, "\n", 1);
-		i++;
-	}
-	child = fork();
-	if (child == -1)
-	{
-		perror("error1");
-	}
-	if (child == 0)
-	{
-		c = execve(argv[0], argv, NULL);
-			if (c == -1)
+		for (i = 0; buffer[i] != 0; i++)
+		{ /*this is to go to the end of the arguments, when the enter is done a new line is added so it is necesary to convert this new line (i-1) in a null*/
+		}
+		buffer[i - 1] = '\0';
+		ptr = strtok(buffer, " ");
+		i = 0;
+		while (ptr)
+		{
+			argv[i] = malloc(1024);
+			argv[i] = ptr;
+			ptr = strtok(NULL, " ");
+			i++;
+		}
+		argv[i] = NULL;
+		child = fork();
+		if (child == -1)
+		{
+			perror("error1");
+		}
+		if (child == 0)
+		{
+			if(execve(argv[0], argv, NULL) == -1) /*it is
+								 doing both things
+								 at the same time.*/
 			{
 				perror("error2");
 				free(buffer);
 				free(argv);
-				return(1);
+				exit(1);
 			}
-	}
-	else
-		wait(&status);
+		}
+		else
+			wait(&status);
 	}
 	free(argv);
 	free(buffer);
