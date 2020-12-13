40cacd383cf476063a938c32be21a0e9ac93d203
Atenea <atecast@gmail.com>
2020-12-13 22:54:33 +0000

    adding length

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,17 +14,25 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 	dlistint_t *tmp = *h, *prev = NULL;
 
+	if (h == NULL)
+		return (NULL);
+	if (idx > dlistint_len(*h))
+		return (NULL);
 	if (*h == NULL || idx == 0)
 	{
 		newnode = add_dnodeint(h, n);
 		return (newnode);
 	}
-
-	for (i = 0; i < idx; i++)
-		tmp = tmp->next;
+	if (idx == dlistint_len(*h))
+	{
+		newnode = add_dnodeint_end(h, n);
+		return (newnode);
+	}
 	newnode = malloc(sizeof(dlistint_t));
 	if (newnode == NULL)
 		return (NULL);
+	for (i = 0; i < idx; i++)
+		tmp = tmp->next;
 	newnode->n = n;
 	prev = tmp->prev;
 	newnode->next = tmp;
@@ -32,11 +40,23 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	prev->next = newnode;
 	tmp->prev = newnode;
 	return (newnode);
+}
+/**
+ * dlistint_len - to returns the number of elements in a linked list
+ * @h: constant rececived
+ * Return: int.
+ */
 
-	if (i == idx)
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h)
 	{
-		newnode = add_dnodeint_end(h, n);
-		return (newnode);
+		count++;
+		h = h->next;
 	}
-	return (NULL);
+	return (count);
 }
