6e2f58ed8c4cf6d802b1ea9790ef9224fe032191
AteCastillo <atecast@gmail.com>
2021-04-20 12:40:05 -0300

    fix 102

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,4 +9,5 @@ int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int binary_search_aux(int *array, int value, int start, int end, int size);
 int int_search_aux(int *arr, int start, int end, size_t size, int value);
+int interpolation_search(int *arr, size_t size, int value);
 #endif
