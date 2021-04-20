cfbe2ff42d1bef66cb990e18fdc57cd0eb027442
AteCastillo <atecast@gmail.com>
2021-04-20 12:21:37 -0300

    fix header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,5 +7,5 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
-int binary_search_aux(int *array, int value, int start, int end, int size)
+int binary_search_aux(int *array, int value, int start, int end, int size);
 #endif
