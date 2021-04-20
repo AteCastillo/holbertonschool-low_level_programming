f62fbd1925bdafb7f7a6a40329c2ec1c92bef2e3
AteCastillo <atecast@gmail.com>
2021-04-20 11:26:16 -0300

    adding new tasks

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,25 @@
+#include "0x1E-search_algos.h"
+
+/**
+ * binary_search - search algorithms in a binary way
+ * array: pointer to the first element of the array
+ * size: size of the array
+ * value: value to search for
+ * return: the index
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+    if (array == NULL)
+        return (-1);
+
+    if (size == 0)
+        return (1);
+    else
+    {   
+        newsize = (size / 2)
+        if (array[index] == value)
+        return (index);
+    }
+    return (-1);
+}
