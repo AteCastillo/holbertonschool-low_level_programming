5907a89d3e7d8e263a7595514697ec4b45e00c80
AteCastillo <atecast@gmail.com>
2021-04-20 12:20:48 -0300

    fix header

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -19,9 +19,7 @@ int linear_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array [%zu] = [%d]\n", index, array[index]);
 		if (array[index] == value)
-		{
 			return (index);
-		}
 	}
 	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,25 +1,42 @@
-#include "0x1E-search_algos.h"
+#include "search_algos.h"
 
-/**
- * binary_search - search algorithms in a binary way
- * array: pointer to the first element of the array
- * size: size of the array
- * value: value to search for
- * return: the index
- */
-
-int binary_search(int *array, size_t size, int value)
+/** binary_search - searches for a value in an array of integers using the Binary search algorithm.
+*@array: array to search.
+*@size: size of the array.
+*@value: value to search into array.
+*Return: first index when value is located in array otherwise return -1.
+*/
+int binary_search(int *array, size_t size, int value) 
 {
-    if (array == NULL)
+    int start = 0, end = (int)size - 1;
+
+    if (!array)
         return (-1);
 
-    if (size == 0)
-        return (1);
-    else
-    {   
-        newsize = (size / 2)
-        if (array[index] == value)
-        return (index);
+    return (binary_search_aux(array, value, start, end, size));
+    
+}
+
+int binary_search_aux(int *array, int value, int start, int end, int size)
+{
+    int mid = 0, i = 0;
+
+    printf("Searching in array: ");
+    for (i = start; i <= end; i++){
+        printf("%d",array[i]);
+        printf(i == end ? "\n" : ", ");  
     }
+    if (size == 0)
+        return (-1);
+    
+    mid = start + (end - start) / 2;
+
+    if (array[mid] == value)
+        return (mid);
+    if (value > array[mid])
+        return (binary_search_aux(array, value, mid + 1, end, (size - 1) / 2));
+    if (value < array[mid])
+        return (binary_search_aux(array, value, start, mid - 1, (size - 1 ) / 2));
+    
     return (-1);
 }
