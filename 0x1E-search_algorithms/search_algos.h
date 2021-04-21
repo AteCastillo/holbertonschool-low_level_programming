c3e96ece2f4eecadcab0fd8b00c598178b3bc16d
AteCastillo <atecast@gmail.com>
2021-04-21 13:55:16 -0300

    add 103

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,4 +12,5 @@ int int_search_aux(int *arr, int start, int end, size_t size, int value);
 int interpolation_search(int *arr, size_t size, int value);
 int advanced_binary_aux(int *array, int value, int start, int end);
 int advanced_binary(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 #endif
